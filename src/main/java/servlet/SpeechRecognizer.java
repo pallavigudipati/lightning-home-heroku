d6ee52abee4415c3b310fa0b222b83541ec63a1b
Pallavi Gudipati <pallavigudipati@gmail.com>
2016-12-23 15:24:17 -0800

    Testing lightiningHome jar

--- src/main/java/servlet/SpeechRecognizer.java
+++ src/main/java/servlet/SpeechRecognizer.java
@@ -10,6 +10,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import edu.cmu.sphinx.api.Configuration;
+import lightningHome.SpellRecognition;
 
 @WebServlet(
         name = "SpeechRecognizer", 
@@ -21,10 +22,12 @@ public class SpeechRecognizer extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
         ServletOutputStream out = resp.getOutputStream();
-        out.write("hello speech".getBytes());
-        String outStr = edu.cmu.sphinx.linguist.allphone.AllphoneLinguist.PROP_ACOUSTIC_MODEL;
+        out.write("hello speech\n".getBytes());
+        String outStr = edu.cmu.sphinx.linguist.allphone.AllphoneLinguist.PROP_ACOUSTIC_MODEL + "\n";
         out.write(outStr.getBytes());
+        String testStr = SpellRecognition.getConfiguration().getDictionaryPath();
+        out.write(testStr.getBytes());
         out.flush();
         out.close();
-    }   
+    }
 }
